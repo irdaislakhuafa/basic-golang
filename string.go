ca9ffe0b40889e84ce296be17e3c1f8b9eda436d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 02:57:26 +0700

    use len() function to count length of string and accessed some character in string with index

--- string.go
+++ string.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Irda")
 	fmt.Println("Irda Islakhu")
-	fmt.Println("Irda Islakhu Afa")
+	fmt.Println("Irda Islakhu Afa\n")
+
+	// use some function string in go
+	fmt.Println(len("Irda"))
+	fmt.Println("Irda Islakhu"[0])
+	fmt.Println("Irda Islakhu Afa"[1])
 }

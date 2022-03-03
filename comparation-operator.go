ff5830836bb0ccefbe574fc6e3c2da5c220229bd
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 04:03:52 +0700

    operator comparison with golang

---
+++ comparation-operator.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	name1 := "irda"
+	name2 := "ana"
+
+	println("name1 == name2 :", (name1 == name2))
+
+	name3 := "irda"
+	println("name1 == name3 :", (name1 == name3))
+
+	println("name1 <= name2 :", (name1 <= name2))
+	println("name1 != name2 :", (name1 != name2))
+}

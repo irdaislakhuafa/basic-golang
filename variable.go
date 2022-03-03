01bec45487f5a83c153b965354bca71e70f206d8
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 03:13:48 +0700

    create more than two variable with golang

--- variable.go
+++ variable.go
@@ -18,4 +18,16 @@ func main() {
 
 	var age = 20
 	fmt.Println("Usia : ", age)
+
+	country := "indonesia" // just for first declaration
+	fmt.Println("Saya tinggal di : ", country, " haha")
+	country = "palembang"
+	fmt.Println("Saya tinggal di : ", country, " haha")
+
+	// create two or more variable with go
+	var (
+		firstName = "irda"
+		lastName  = "islakhu afa"
+	)
+	fmt.Println("Hello my name is ", firstName, lastName, "see you")
 }

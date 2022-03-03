380e9233fbc3ab5730afa0418e8da63aada90b27
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 04:29:57 +0700

    create first array in golang

---
+++ array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "irda"
+	names[1] = "islakhu"
+	names[2] = "afa"
+
+	fmt.Println(names)
+	fmt.Println(len(names))
+}

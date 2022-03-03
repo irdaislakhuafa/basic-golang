4cafe0af44a1d43a7e969b090792eaff6c96e4ee
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 03:04:05 +0700

    create variable with type string

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Irda Islakhu Afa"
+	fmt.Println("My name is : ", name)
+
+	name = "Ana Ardani"
+	fmt.Println("My name is : ", name)
+
+	// name = true -> error
+}

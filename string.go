db36c02fb336c259f80714031fefbf212398cd44
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 02:58:29 +0700

    modify something

--- string.go
+++ string.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Irda")
 	fmt.Println("Irda Islakhu")
-	fmt.Println("Irda Islakhu Afa\n")
+	fmt.Println("Irda Islakhu Afa\n ")
 
 	// use some function string in go
 	fmt.Println(len("Irda"))

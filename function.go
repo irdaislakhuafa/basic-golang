9348232b463fc1f6d065d82f0a8410adb0510c42
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 15:53:11 +0700

    function with params

--- function.go
+++ function.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("\033\143")
-	for i := 0; i < 10; i++ {
-		sayHello()
-	}
+	// for i := 0; i < 10; i++ {
+	// 	sayHello(string(i))
+	// }
+	sayHello("irda", "islakhu afa")
 }
-func sayHello() {
-	fmt.Println("Hello")
+
+// func sayHello() {
+// 	fmt.Println("Hello")
+// }
+func sayHello(firstName string, lastName string) {
+	fmt.Println("Hello", firstName, lastName)
 }

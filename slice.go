2453410629c8f122b7bd6408a8dddd6fc5361f7a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 10:28:43 +0700

    create slice from array in golang

---
+++ slice.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// slice mereference ke array
+	// jika array di buah maka data didalam slice juga akan berubah begitupun sebaliknya
+	// slice mirip sub array / sub list kalau dijava
+	month := [...]string{
+		"januari",
+		"februari",
+		"maret",
+		"april",
+		"mei",
+		"juni",
+		"juli",
+		"agustus",
+		"september",
+		"oktober",
+		"november",
+		"desember",
+	}
+
+	slice1 := month[3:7]
+	fmt.Println("Slice1 :", slice1)
+
+	// beberapa function di slice
+	// - len(slice) -> untuk medapatkan panjang/length slice
+	// - cap(slice) -> untuk mendapatkan total kapasitas slice (cap for capacity)
+	// - append(slice, data) -> membuat slice baru dengan menambah data ke posisi terakhir slice, jika kapasitas sudah penuh maka akan dibuat slice baru
+	// - make([]TypeData, length, capacity) -> untuk membuat slice baru
+	// - copy(destination, source) -> mengcopy/menyalin slice ke slice lain
+	fmt.Println("Length slice1 :", len(slice1))
+	fmt.Println("Capacity slice1 :", cap(slice1))
+
+	fmt.Println("Merubah slice ke 5 :")
+	slice1[1] = "(ana ardani)"
+	fmt.Println(slice1)
+	month[5] = "(ini di ubah)"
+	fmt.Println(slice1)
+}

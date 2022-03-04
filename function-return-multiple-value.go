80dba3ca04c6b1269c6eca267cd281d254e6599b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 16:05:32 +0700

    use function with return multiple value in golang

--- function-return-multiple-value.go
+++ function-return-multiple-value.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	firstName, lastName := getFullName()
-	fmt.Println(firstName, lastName)
+	// firstName, lastName := getFullName()
+
+	// if want to ignore result use _
+	firstName, _ := getFullName()
+
+	// fmt.Println(firstName, lastName)
+	fmt.Println(firstName)
+	fmt.Println(getFullName())
 }
+
+// function with return multiple value
 func getFullName() (string, string) {
 	return "irda", "islakhu afa"
 }

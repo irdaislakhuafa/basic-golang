81661140e1b7080f9d7da5a4e33c9a0c9abca582
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-04 15:34:27 +0700

    learn how to use for range (slice, array, map)

--- for.go
+++ for.go
@@ -16,6 +16,31 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("Perulangan ke :", (i + 1))
 	}
+	line(30)
+
+	// access slice with for
+	slice := []string{"irda", "islakhu", "afa"}
+	for i := 0; i < len(slice); i++ {
+		fmt.Println("data ke :", i, "adalah = ", slice[i])
+	}
+	line(30)
+
+	// access slice with for range
+	for index, value := range slice {
+		fmt.Println("index ke", index, "nilainya :", value)
+	}
+	line(30)
+
+	// access map with for range
+	mapExmaple := map[string]string{
+		"firstName":  "irda",
+		"middleName": "islakhu",
+		"lastName":   "afa",
+	}
+
+	for key, value := range mapExmaple {
+		fmt.Printf("key : %s, value : %s\n", key, value)
+	}
 }
 
 func line(lineLong int) {

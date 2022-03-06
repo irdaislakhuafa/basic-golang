c2dfd64362126baa327b6da887926b676aee32d5
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-06 16:11:14 +0700

    create package and import package in golang

---
+++ helpers/package.go
@@ -0,0 +1,15 @@
+package helpers
+
+import (
+	"fmt"
+)
+
+func SayHelloFromHelper() {
+	fmt.Println("Hello :D")
+}
+
+/*
+PACKAGE : mirip seperti package di java, dan harus membuat folder yang sama sengan stuctur package
+- adalah tempat yang bisa digunakan untuk mengorganisir kode program yang kita buat di Go-Lang
+- di 1 package tidak boleh ada function/struct yang sama
+*/
---
+++ import.go
@@ -0,0 +1,13 @@
+package main
+
+import "basic-golang/helpers"
+
+func main() {
+	helpers.SayHelloFromHelper()
+}
+
+/*
+IMPORT : mirip import di java
+- secara default file Go hanya bisa mengakses file Go lainya di package yang sama
+- jika ingin mengakses file Go di package yang berbeda maka harus dilakukan import
+*/
